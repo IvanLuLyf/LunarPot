435de54ae931c281a5094fb45aea64d118710abb
IvanLuLyf <835498692@qq.com>
2021-03-01 23:02:49 +0800

    Initial Commit

---
+++ src/main/java/cn/twimi/live/OneLiveApplication.java
@@ -0,0 +1,13 @@
+package cn.twimi.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/twimi/live/OneLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.twimi.live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
