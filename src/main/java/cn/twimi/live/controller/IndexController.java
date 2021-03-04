2e064dae5e59757c8324d5b18cf509bbc212940c
IvanLuLyf <835498692@qq.com>
2021-03-04 23:47:56 +0800

    update version

--- src/main/java/cn/twimi/live/controller/IndexController.java
+++ src/main/java/cn/twimi/live/controller/IndexController.java
@@ -9,6 +9,6 @@ public class IndexController {
     @RequestMapping(value = {"/", "/index"})
     @ResponseBody
     public String index() {
-        return "OneLive 20210303";
+        return "OneLive 20210304";
     }
 }
