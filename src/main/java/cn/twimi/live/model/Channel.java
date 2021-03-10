1fde35dc4d0acc8fc6c9037761420f8c8e5e04b5
IvanLuLyf <835498692@qq.com>
2021-03-10 23:08:24 +0800

    update channel model

--- src/main/java/cn/twimi/live/model/Channel.java
+++ src/main/java/cn/twimi/live/model/Channel.java
@@ -2,9 +2,24 @@ package cn.twimi.live.model;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Channel {
     private long id;
     private String title;
     private long userId;
+    private Date createTime;
+    private int state;
+
+    public Channel() {
+
+    }
+
+    public Channel(long userId, String title) {
+        this.userId = userId;
+        this.title = title;
+        this.createTime = new Date();
+        this.state = 0;
+    }
 }
