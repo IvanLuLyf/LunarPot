4a413f4aa73d2e450a3a2fe1afef954d7e8ff165
IvanLuLyf <835498692@qq.com>
2021-03-15 00:06:39 +0800

    add CacheAble

--- src/main/java/cn/twimi/live/controller/FileController.java
+++ src/main/java/cn/twimi/live/controller/FileController.java
@@ -3,11 +3,13 @@ package cn.twimi.live.controller;
 import cn.twimi.live.util.IPFSUtil;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 @CrossOrigin
@@ -15,9 +17,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class FileController {
     @RequestMapping(value = "/p/{id}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     @ResponseBody
-    @Cacheable("pictures")
-    public byte[] picture(@PathVariable("id") String id) {
-        return IPFSUtil.fetchFile(id);
+    @Cacheable(value = "pictures", key = "#id")
+    public ResponseEntity<byte[]> picture(@PathVariable("id") String id) {
+        return ResponseEntity.ok().lastModified(System.currentTimeMillis()).body(IPFSUtil.fetchFile(id));
     }
 
     @RequestMapping(value = "/t/{id}", produces = MediaType.TEXT_PLAIN_VALUE)
