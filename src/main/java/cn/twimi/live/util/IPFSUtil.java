c472a6300bcb3e953c1c6e693953d35d2d68c6a8
IvanLuLyf <835498692@qq.com>
2021-03-14 21:04:39 +0800

    add IPFSUtil

---
+++ src/main/java/cn/twimi/live/util/IPFSUtil.java
@@ -0,0 +1,40 @@
+package cn.twimi.live.util;
+
+import io.ipfs.api.IPFS;
+import io.ipfs.api.MerkleNode;
+import io.ipfs.api.NamedStreamable;
+import io.ipfs.multihash.Multihash;
+
+import java.io.IOException;
+import java.util.List;
+
+public class IPFSUtil {
+    private static IPFS ipfs;
+
+    private static IPFS getInstance() {
+        if (ipfs == null) {
+            ipfs = new IPFS("/dnsaddr/ipfs.infura.io/tcp/5001/https");
+        }
+        return ipfs;
+    }
+
+    public static String uploadFile(byte[] content) {
+        NamedStreamable.ByteArrayWrapper wrapper = new NamedStreamable.ByteArrayWrapper(content);
+        try {
+            List<MerkleNode> result = getInstance().add(wrapper);
+            return result.get(0).hash.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static byte[] fetchFile(String hash) {
+        try {
+            return getInstance().cat(Multihash.fromBase58(hash));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
