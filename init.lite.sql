951b31af12ceceeaf6b754bda2b8ac81616fede7
IvanLuLyf <835498692@qq.com>
2021-04-04 15:38:22 +0800

    update SQL

--- init.lite.sql
+++ init.lite.sql
@@ -19,13 +19,24 @@ create table tp_user
 insert into tp_user (id, username, password, email, phone, role_id, state)
 values (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@twimi.cn', '13312345678', 1, 1);
 
-create table tp_channel
+create table tp_live
 (
     id          integer
-        constraint tp_channel_pk
+        constraint tp_live_pk
             primary key autoincrement,
     title       varchar(50) not null,
     create_time datetime,
     user_id     integer,
     state       integer
+);
+
+create table tp_message
+(
+    id        integer
+        constraint tp_message_pk
+            primary key autoincrement,
+    type      integer,
+    content   text,
+    extra     text,
+    timestamp datetime
 );
--- init.pg.sql
+++ init.pg.sql
@@ -21,13 +21,24 @@ comment on table tp_user is '用户表';
 insert into tp_user (id, username, password, email, phone, role_id, state)
 values (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@twimi.cn', '13312345678', 1, 1);
 
-create table tp_channel
+create table tp_live
 (
     id          serial
-        constraint tp_channel_pk
+        constraint tp_live_pk
             primary key,
     title       varchar(50) not null,
     create_time timestamp,
     user_id     integer,
     state       integer
+);
+
+create table tp_message
+(
+    id        serial
+        constraint tp_message_pk
+            primary key,
+    type      integer,
+    content   text,
+    extra     text,
+    timestamp timestamp
 );
