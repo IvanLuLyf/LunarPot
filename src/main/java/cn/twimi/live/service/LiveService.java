d67f923f0027197e86c3f16aeaed24d6ad5fb105
IvanLuLyf <835498692@qq.com>
2021-04-04 22:55:12 +0800

    add push

--- src/main/java/cn/twimi/live/service/LiveService.java
+++ src/main/java/cn/twimi/live/service/LiveService.java
@@ -2,9 +2,17 @@ package cn.twimi.live.service;
 
 import cn.twimi.live.common.ApiResponse;
 import cn.twimi.live.model.Live;
+import cn.twimi.live.model.Message;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public interface LiveService {
     ApiResponse<Live> create(Live live);
+
+    ApiResponse<Message> push(String liveId, String content);
+
+    ApiResponse<Message> push(String liveId, String content, MultipartFile file);
+
+    ApiResponse<Message> close(String liveId);
 }
--- src/main/java/cn/twimi/live/service/impl/LiveServiceImpl.java
+++ src/main/java/cn/twimi/live/service/impl/LiveServiceImpl.java
@@ -2,18 +2,31 @@ package cn.twimi.live.service.impl;
 
 import cn.twimi.live.common.ApiResponse;
 import cn.twimi.live.dao.LiveDao;
+import cn.twimi.live.dao.MessageDao;
+import cn.twimi.live.model.FileInfo;
 import cn.twimi.live.model.Live;
+import cn.twimi.live.model.Message;
+import cn.twimi.live.service.FileService;
 import cn.twimi.live.service.LiveService;
+import cn.twimi.live.util.IdUtil;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Service
 public class LiveServiceImpl implements LiveService {
     private final LiveDao liveDao;
+    private final SimpMessageSendingOperations operations;
+    private final FileService fileService;
+    private final MessageDao messageDao;
 
-    public LiveServiceImpl(LiveDao liveDao) {
+    public LiveServiceImpl(LiveDao liveDao, FileService fileService, SimpMessageSendingOperations operations, MessageDao messageDao) {
         this.liveDao = liveDao;
+        this.fileService = fileService;
+        this.operations = operations;
+        this.messageDao = messageDao;
     }
 
     @Override
@@ -22,10 +35,36 @@ public class LiveServiceImpl implements LiveService {
         if (lives.size() > 0) {
             return ApiResponse.<Live>builder().status(1).msg("已有正在进行的直播").build();
         }
-        long row = liveDao.create(live);
+        int row = liveDao.create(live);
         if (row <= 0) {
             return ApiResponse.<Live>builder().status(-6).msg("数据库出错").build();
         }
         return ApiResponse.<Live>builder().status(0).msg("ok").data(live).build();
+    }
+
+    @Override
+    public ApiResponse<Message> push(String liveId, String content) {
+        return this.push(liveId, content, null);
+    }
+
+    @Override
+    public ApiResponse<Message> push(String liveId, String content, MultipartFile file) {
+        Message message = new Message(IdUtil.decode(liveId), content);
+        if (file != null) {
+            message.setType(2);
+            FileInfo fileInfo = fileService.upload(file, "image");
+            message.setExtra(fileService.pathToUrl(fileInfo.getPath()));
+        }
+        int row = messageDao.create(message);
+        if (row <= 0) {
+            return ApiResponse.<Message>builder().status(-6).msg("数据库出错").build();
+        }
+        this.operations.convertAndSend("/topic/channel/" + liveId, message);
+        return ApiResponse.<Message>builder().data(message).msg("ok").status(0).build();
+    }
+
+    @Override
+    public ApiResponse<Message> close(String liveId) {
+        return null;
     }
 }
