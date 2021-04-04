7a45d266df75ccb24db29dd2b57c4857d779f5fb
IvanLuLyf <835498692@qq.com>
2021-04-04 16:04:43 +0800

    change name and add create Live

--- src/main/java/cn/twimi/live/controller/LiveController.java
+++ src/main/java/cn/twimi/live/controller/LiveController.java
@@ -3,8 +3,11 @@ package cn.twimi.live.controller;
 import cn.twimi.live.annotation.Permission;
 import cn.twimi.live.common.ApiResponse;
 import cn.twimi.live.model.FileInfo;
+import cn.twimi.live.model.Live;
 import cn.twimi.live.model.Message;
+import cn.twimi.live.model.User;
 import cn.twimi.live.service.FileService;
+import cn.twimi.live.service.LiveService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -12,21 +15,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Controller
-@RequestMapping("/api/channel")
+@RequestMapping("/api/live")
 public class LiveController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     private final SimpMessageSendingOperations operations;
     private final FileService fileService;
+    private final LiveService liveService;
 
-    public LiveController(SimpMessageSendingOperations operations, FileService fileService) {
+    public LiveController(SimpMessageSendingOperations operations, FileService fileService, LiveService liveService) {
         this.operations = operations;
         this.fileService = fileService;
+        this.liveService = liveService;
     }
 
+    @Permission("host")
+    @PostMapping("/create")
+    @ResponseBody
+    public ApiResponse<Live> createLive(
+            HttpServletRequest request,
+            @RequestParam(value = "title") String title
+    ) {
+        User user = (User) request.getAttribute("curUser");
+        return liveService.create(new Live(user.getId(), title));
+    }
 
     @Permission("host")
     @PostMapping("/push/{room}")
