751bf212451a6398ae0356ef8ac5b83bcef15552
IvanLuLyf <835498692@qq.com>
2021-04-04 22:45:25 +0800

    update Message.java

--- src/main/java/cn/twimi/live/model/Message.java
+++ src/main/java/cn/twimi/live/model/Message.java
@@ -1,5 +1,6 @@
 package cn.twimi.live.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.util.Date;
@@ -7,8 +8,24 @@ import java.util.Date;
 @Data
 public class Message {
     private long id;
+    @JsonIgnore
+    private long liveId;
     private int type;
     private String content;
     private String extra;
     private Date timestamp;
+
+    public static final int TYPE_SYSTEM = 0;
+    public static final int TYPE_TEXT = 1;
+    public static final int TYPE_IMAGE = 2;
+
+    public Message() {
+
+    }
+
+    public Message(long liveId, String content) {
+        this.liveId = liveId;
+        this.content = content;
+        this.timestamp = new Date();
+    }
 }
