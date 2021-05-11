cd926a27ea48bf110eb03eabbfadb93c3873c628
IvanLuLyf <835498692@qq.com>
2021-05-11 23:02:34 +0800

    add Entry

--- src/main/java/cn/twimi/live/controller/IndexController.java
+++ src/main/java/cn/twimi/live/controller/IndexController.java
@@ -2,13 +2,16 @@ package cn.twimi.live.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
     @RequestMapping({"/", "/index"})
-    @ResponseBody
     public String index() {
-        return "OneLive 20210505";
+        return "index.html";
+    }
+
+    @RequestMapping({"/admin"})
+    public String admin() {
+        return "admin.html";
     }
 }
