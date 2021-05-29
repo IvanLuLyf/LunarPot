7291982d45c57a2fe820e2eb901a85ea5a48f1f3
IvanLuLyf <835498692@qq.com>
2021-05-29 15:38:42 +0800

    update AttachController

--- src/main/java/cn/twimi/live/controller/AttachController.java
+++ src/main/java/cn/twimi/live/controller/AttachController.java
@@ -5,7 +5,10 @@ import cn.twimi.live.common.ApiResponse;
 import cn.twimi.live.model.FileInfo;
 import cn.twimi.live.model.User;
 import cn.twimi.live.service.FileService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -13,9 +16,15 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin
 public class AttachController {
 
+    private final FileService fileService;
+
+    public AttachController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
     @Permission(User.LOGIN)
     @RequestMapping("/upload")
-    public ApiResponse<String> upload(@RequestParam("file") MultipartFile file, FileService fileService) {
+    public ApiResponse<String> upload(@RequestParam("file") MultipartFile file) {
         FileInfo fileInfo = fileService.upload(file, "attach");
         return ApiResponse.<String>builder()
                 .status(0)
